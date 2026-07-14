16ebf475fbb014fc3d6bfc2d9ae88474e28a12f6
glsimayy <canpolatgulsima02@gmail.com>
2026-07-14 17:04:02 +0300

    Delete backend directory

--- backend/server.js
+++
@@ -1,9 +0,0 @@
-// backend/server.js
-const { poolPromise } = require('./db');
-
-async function startServer() {
-    await poolPromise;
-    console.log("Sunucu hazır ve veritabanı bağlantısı kuruldu.");
-}
-
-startServer();
